13491edc6655c807afeff764563690478430e816
rigel <rigel.k@kakaocorp.com>
2016-11-08 11:53:54 +0900

    Add heart

--- python/heart.py
+++ python/heart.py
@@ -3,19 +3,9 @@ import requests
 
 payload = '{"name": "aa", "text": "bbb"}'
 while True :
-    # ser = serial.Serial('/dev/ttyUSB0')  # open serial port
     ser = serial.Serial('/dev/tty.usbmodem1411')  # open serial port
     line = ser.readline()
-    # line  = ''.join(line.splitlines())
-    # print(line)
-
-    # line = "hello\n"
-
     if line == b'click\r\n':
         # print(line)
         r = requests.post("https://kakong.firebaseio.com/index.json", data=payload)
         # print(r.text)
-
-# print(ser.name)         # check which port was really used
-# ser.write(b'hello')     # write a string
-# ser.close()‰‰
